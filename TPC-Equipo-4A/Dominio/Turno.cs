31a375ba2c7da768e4a444c998603c03ab4cd4d5
Jess Ibañez <jessicavanina18@gmail.com>
2024-10-16 23:07:54 -0300

    Turnos y Estados

---
+++ TPC-Equipo-4A/Dominio/Turno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Turno
+    {
+        public int Id_Turno { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Hora { get; set; }
+        public Paciente Paciente { get; set; }
+        public PersonalMedico Medico { get; set; }
+        public EstadoTurno Estado { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
