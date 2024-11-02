ddbe4bf174c82e686d5aeb1cfb320b9e5f93cfc8
Jess Ibañez <jessicavanina18@gmail.com>
2024-11-01 22:13:07 -0300

    Varios

--- TPC-Equipo-4A/Dominio/Persona.cs
+++ TPC-Equipo-4A/Dominio/Persona.cs
@@ -8,11 +8,7 @@ namespace Dominio
 {
     public class Persona
     {
-        //public DateTime FechaNacimiento { get; set; }
-        //public int Dni { get; set; }
-        //public string Email { get; set; }
-        //public Domicilio Domicilio { get; set; }
-        //public int Telefono { get; set; }
+        
         public int Id_Persona { get; set; }
         public string NombreYApellido { get; set; }
         public string Telefono { get; set; }
