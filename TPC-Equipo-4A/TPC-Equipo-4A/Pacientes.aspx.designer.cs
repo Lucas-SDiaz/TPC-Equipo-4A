ef5bf160f8b4d190d7d0f4e851e4d57f0cb11513
Lucas Santiago Diaz <lucas.santiago.diaz@gmail.com>
2024-11-19 19:46:05 -0300

    DB_Actualizada

--- TPC-Equipo-4A/TPC-Equipo-4A/Pacientes.aspx.designer.cs
+++ TPC-Equipo-4A/TPC-Equipo-4A/Pacientes.aspx.designer.cs
@@ -67,5 +67,14 @@ namespace TPC_Equipo_4A
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView dgvPacientes2;
+
+        /// <summary>
+        /// Control dgvPacientesMedicos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dgvPacientesMedicos;
     }
 }
